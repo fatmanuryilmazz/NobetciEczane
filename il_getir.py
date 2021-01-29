4e10d35291306d5128235f5958541845b74688c7
Ugurcan Dede <ugurcan.dede@outlook.com.tr>
2021-01-30 01:46:31 +0300

    il ilçe fonksiyonlara ayrıldı

--- il_getir.py
+++ il_getir.py
@@ -3,12 +3,23 @@ import json
 with open('il.json',encoding='utf8') as f:
     veri = json.load(f)
 
-il=input("hangi il?\n")
-
-
-data=veri["iller"][il.capitalize()]
-
+iller=[]
 ilce=[]
-for il in data:
-    ilce.append(il)
-print(ilce)
+
+data=veri["iller"]
+
+def get_il():
+    for il in data.keys():
+        iller.append(il)
+    print(iller)
+
+
+def get_ilce(il):
+    for il in data[il.capitalize()]:
+        ilce.append(il)
+    print(ilce)
+
+get_il()
+
+il=input("hangi il?\n")
+get_ilce(il)
